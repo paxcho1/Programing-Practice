1b7c745d4f0b4154c771f200f3f37dc7611014d7
paxcho1 <paxcho1@naver.com>
2017-09-25 16:36:30 +0900

    Update 소스.c

--- ConsoleApplication9/소스.c
+++ ConsoleApplication9/소스.c
@@ -2,18 +2,18 @@
 
 char *reverse(char *str1);
 char *encryptN(char *str1, int n);
-char *rotateN(char *str1);
+char *rotateN(char *str1,int n);
 void print(char *str1, int n);
 
 int main(){
-
-	char Line[101] = { NULL};
+	
+	char Line[101] = { NULL };
 	int Count = 0;
 	int Rotate = 0;
 	int Encrypt = 0;
 
-	printf("입력을 종료하려면 새로운 행에서 (ctrl + z)를 누르십시오.\n");
-	
+	printf("문자열을 입력해 주십시오.\n");
+
 	gets(Line);
 
 	while (Line[Count]){
@@ -29,23 +29,24 @@ int main(){
 
 	//배열 정상화
 	reverse(Line);
-	
 
+	//배열 Encrypt
 	printf("몇 만큼 Encyrpt?:");
 	scanf("%d", &Encrypt);
-	printf("\nEncrypt:");
-	
+	printf("Encrypt:");
+
 	print(encryptN(Line, Encrypt), Count);
-/*
+	
+	//배열 정상화
+	print(encryptN(Line, -Encrypt), Count);
+	/*
 	printf("몇 만큼 Rotate?:");
 	printf("Rotate:");
 	scanf("%d", &Rotate);
 	print(rotate(Line, Rotate), Count);
 	*/
 
-
-
-	
+	return 0;
 }
 
 void print(char *str1, int n){
@@ -61,12 +62,12 @@ char *reverse(char *str1){
 	while (str1[Count]){
 		Count++;
 	}
-	for (int i = 0; i <((Count)/2); i++){
+	for (int i = 0; i <((Count) / 2); i++){
 		Temp = str1[i];
-		str1[i] = str1[(Count-1)-i];
-		str1[(Count-1) - i] = Temp;
-		
-		
+		str1[i] = str1[(Count - 1) - i];
+		str1[(Count - 1) - i] = Temp;
+
+
 	}
 
 	return str1;
@@ -77,16 +78,42 @@ char *encryptN(char *str1, int n){
 
 	while (str1[Count]){
 		Count++;
+		
 	}
-	for (int i = 0; i < Count-1; i++){
+	if (n > 26){
+		n = n % 26;
+	}
+	else if (n < 0 &&n>=-26){
+		n = n + 26;
+	}
+	else if (n < -26){
+		n = -(-n % 26)+26;
+	}
+	for (int i = 0; i < Count; i++){
 		if (str1[i] >= 65 && str1[i] <= 90){
 			
-			str1[i] = str1[i]+n;
+			str1[i] = str1[i] + n;
+			if (str1[i]>90){
+				str1[i] = str1[i] - 26;
+			}
 		}
 		else if (str1[i] >= 97 && str1[i] <= 122){
-			str1[i] = str1[i + n];
+			str1[i] = str1[i] + n;
+			if (str1[i] > 122){
+				str1[i] = str1[i] - 26;
+			}
 		}
 
 	}
 	return str1;
+}
+char *rotateN(char *str1, int n){
+	int Count = 0;
+	while (str1[Count]){
+		Count++;
+	}
+	for (int i = 0; i < Count; i++){
+
+	}
+
 }
