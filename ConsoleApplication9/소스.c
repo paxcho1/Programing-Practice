04448955e8a6176cced0398ba73f7cb343944c4a
paxcho1 <paxcho1@naver.com>
2017-09-26 13:39:22 +0900

    Update 소스.c

--- ConsoleApplication9/소스.c
+++ ConsoleApplication9/소스.c
@@ -5,64 +5,72 @@ char *encryptN(char *str1, int n);
 char *rotateN(char *str1, int n);
 void printString(char *str1, int n);
 
-int main(){
-	
+int main() {
+
 	char Line[101] = { NULL };
 	int Count = 0;
 	int Rotate = 0;
 	int Encrypt = 0;
 
 	printf("문자열을 입력해 주십시오.\n");
-
+	
+	//문자열 입력받기
 	gets(Line);
 
-	while (Line[Count]){
+	//문자열의 문자 갯수 
+	while (Line[Count]) {
 		Count++;
 	}
+	//입력된 문자열 출력
 	printf("입력된 String:");
 	printString(Line, Count);
 
 
-	//배열 Reverse
+	//문자열 Reverse
 	printf("Reversed:");
 	printString(reverse(Line), Count);
 
-	//배열 정상화
+	//문자열 정상화
 	reverse(Line);
 
-	//배열 Encrypt
+	//문자열 Encrypt
 	printf("몇 만큼 Encyrpt?:");
 	scanf("%d", &Encrypt);
 	printf("Encrypt:");
 
 	printString(encryptN(Line, Encrypt), Count);
 
-	//배열 정상화
+	//문자열 정상화
 	encryptN(Line, -Encrypt);
-	
+	//입력 버퍼 지우기
+	fflush(stdin);
+
+	//문자열 Rotate
 	printf("몇 만큼 Rotate?:");
-	printf("Rotate:");
+	
 	scanf("%d", &Rotate);
-	printString(rotate(Line, Rotate), Count);
-	
-	
+
+	printf("Rotate:");
+	printString(rotateN(Line, Rotate), Count);
+
+
 	return 0;
 }
 
-void printString(char *str1, int n){
-	for (int i = 0; i < n; i++){
+void printString(char *str1, int n) {
+	for (int i = 0; i < n; i++) {
 		printf("%c", str1[i]);
 	}
 	printf("\n");
 }
-char *reverse(char *str1){
+char *reverse(char *str1) {
 	char Temp = 0;
 	int Count = 0;
 
-	while (str1[Count]){
+	while (str1[Count]) {
 		Count++;
 	}
-	for (int i = 0; i <((Count) / 2); i++){
+	for (int i = 0; i <((Count) / 2); i++) {
 		Temp = str1[i];
 		str1[i] = str1[(Count - 1) - i];
 		str1[(Count - 1) - i] = Temp;
@@ -73,37 +81,37 @@ char *reverse(char *str1){
 	return str1;
 
 }
-char *encryptN(char *str1, int n){
+char *encryptN(char *str1, int n) {
 	int Count = 0;
 
-	while (str1[Count]){
+	while (str1[Count]) {
 		Count++;
 
 	}
-	if (n > 26 || n<-26){
+	if (n > 26 || n<-26) {
 		n = n % 26;
 	}
-	
-	
-	
-	for (int i = 0; i < Count; i++){
-		if (str1[i] >= 65 && str1[i] <= 90){
+
+
+
+	for (int i = 0; i < Count; i++) {
+		if (str1[i] >= 65 && str1[i] <= 90) {
 
 			str1[i] = str1[i] + n;
 
-			if (str1[i]>90){
+			if (str1[i]>90) {
 				str1[i] = str1[i] - 26;
 			}
-			else if (str1[i]<65){
+			else if (str1[i]<65) {
 				str1[i] = str1[i] + 26;
 			}
 		}
-		else if (str1[i] >= 97 && str1[i] <= 122){
+		else if (str1[i] >= 97 && str1[i] <= 122) {
 			str1[i] = str1[i] + n;
-			if (str1[i] > 122){
+			if (str1[i] > 122) {
 				str1[i] = str1[i] - 26;
 			}
-			else if (str1[i] < 97){
+			else if (str1[i] < 97) {
 				str1[i] = str1[i] + 26;
 			}
 		}
@@ -111,17 +119,19 @@ char *encryptN(char *str1, int n){
 	}
 	return str1;
 }
-char *rotateN(char *str1, int n){
+char *rotateN(char *str1, int n) {
 	int Count = 0;
-	while (str1[Count]){
+	while (str1[Count]) {
 		Count++;
 	}
-	n = n%Count;
-	for (int i = 0; i < Count; i++){
-		str1[i] = str1[i + n];
-		if (i + n>Count){
-			str1[i] = str1[i + n - Count];
+	char Temp = { NULL };
+	for (int i = 0; i < n; i++) {
+		Temp = str1[Count-1];
+		for (int j = Count-1; j > 0; j--){
+			
+			str1[j] = str1[j-1];
 		}
+		str1[0] = Temp;
 	}
 	return str1;
 }
